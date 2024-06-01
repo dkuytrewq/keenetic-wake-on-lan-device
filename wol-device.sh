e06fa304468936bf87a6555b2870ed9ad35fff31
M34T BICYC13 <44604317+dkuytrewq@users.noreply.github.com>
2024-06-02 02:57:20 +0300

    Лог теперь выводится в системный журнал в интерфейсе роутера

--- wol-device.sh
+++ wol-device.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-echo "woLAN - start"
+logger -t woLAN "wake on lan - script start"
 
 ping -c 3 192.168.1.1 > /dev/null
 if [ $? -ne 0 ]; then
   wget -qO - --post-data='[{"mac":"1a:2b:3c:11:22:33"}]' http://localhost:79/rci/ip/hotspot/wake
-  echo "WoLAN - server start"
+  logger -t woLAN "wake on lan - device waked"
 fi
 
-echo "woLAN - end"
+logger -t woLAN "wake on lan - script finish"
